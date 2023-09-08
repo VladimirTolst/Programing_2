70a07f588cd7b70ba0920396d4990bd0c71c9fa5
Владимир <@DESKTOP-M1BP9AF>
2023-09-08 12:49:21 +0700

    Добавьте файлы проекта.

---
+++ Programing_2/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+static bool password_test(string pass_inpt, string password)
+{
+    if (pass_inpt == password)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+string password = "8976";
+int bal = 2000;
+
+Console.WriteLine("Output - 1, input - 2");
+string inpt = Console.ReadLine();
+switch (inpt)
+{
+    case "1":
+        {
+            Console.WriteLine("PIN: ");
+            string pass_inpt = Console.ReadLine();
+            if (password_test(pass_inpt, password))
+            {
+                Console.WriteLine("Balance is " + bal);
+                int bal_out = Convert.ToInt32(Console.ReadLine());
+                if (bal - bal_out >= 0)
+                {
+                    bal -= bal_out;
+                    Console.WriteLine("Balance = " + bal);
+                }
+                else
+                {
+                    Console.WriteLine("Not enough");
+                }
+            }
+            else
+            {
+                Console.WriteLine("wrong pin");
+                break;
+            }
+            break;
+        }
+
+    case "2":
+        {
+            Console.WriteLine("PIN: ");
+            string pass_inpt = Console.ReadLine();
+            if (password_test(pass_inpt, password))
+            {
+                Console.WriteLine("input: ");
+                int bal_in = Convert.ToInt32(Console.ReadLine());
+                bal += bal_in;
+                Console.WriteLine("balance " + bal);
+            }
+            else
+            {
+                Console.WriteLine("wrong pin");
+                break;
+            }
+            break;
+        }
+    default:
+        {
+            Console.WriteLine("incorrect input");
+            break;
+        }
+}
